44f847e03683350f4e51ebf83a2f2319d9f48351
pintofran <francopinto7@gmail.com>
2019-04-25 21:40:20 -0300

    Termina Before and After

--- ruby/lib/prueba.rb
+++ ruby/lib/prueba.rb
@@ -1,8 +1,43 @@
 class Contrato
 
+  @@antes = []
+  @@despues = []
+
   def self.before_and_after_each_call(antes,dps)
-    @@antes = antes
-    @@despues = dps
+    @@antes.push([antes,self])
+    @@despues.push([dps,self])
+  end
+
+  @@new_method = true
+
+  def self.method_added(name)
+    if @@new_method
+      @@new_method = false
+
+      old_methodç = self.instance_method(name)
+
+      self.define_method(name) do |*arg|
+
+        @@antes.each { |tuplaProc|
+
+          if(tuplaProc[1] == self.class)
+            tuplaProc[0].call
+          end
+
+        }
+        var = old_method.bind(self).call
+        @@despues.each { |tuplaProc|
+
+          if(tuplaProc[1] == self.class)
+            tuplaProc[0].call
+          end
+
+        }
+        return var
+
+      end
+      @@new_method = true
+    end
   end
 
 end
@@ -11,45 +46,45 @@ class MiClase < Contrato
 
   before_and_after_each_call(
       # Bloque Before. Se ejecuta antes de cada mensaje
-      proc{ puts 'Entré a un mensaje' },
+      proc{ puts 'Primero aca' },
       # Bloque After. Se ejecuta después de cada mensaje
-      proc{ puts 'Salí de un mensaje' }
+      proc{ puts 'Dps aca' }
   )
 
-  @new_method = true
-
-  def self.method_added(name)
-    if @new_method
-      @new_method = false
-
-      old_method = self.instance_method(name)
-
-      self.define_method(name) do |*arg|
-
-        @@antes.call
-        var = old_method.bind(self).call
-        @@despues.call
-        return var
-
-      end
-      @new_method = true
-    end
-  end
-
   def mensaje_1
-    puts 'Mensaje 1'
+    puts 'Primer prueba'
     return 5
   end
 
-  def mensaje_2
-    puts 'Mensaje 2'
+end
+
+class MiClase2 < Contrato
+
+  before_and_after_each_call(
+      # Bloque Before. Se ejecuta antes de cada mensaje
+      proc{ puts 'Aca otra cosa' },
+      # Bloque After. Se ejecuta después de cada mensaje
+      proc{ puts 'Wiiii' }
+  )
+
+  before_and_after_each_call(
+      # Bloque Before. Se ejecuta antes de cada mensaje
+      proc{ puts 'Re loco' },
+      # Bloque After. Se ejecuta después de cada mensaje
+      proc{ puts 'Seee' }
+  )
+
+  def mensaje_1
+    puts 'Segunda prueba'
     return 3
   end
 
 end
 
-var = MiClase.new.mensaje_3
-pp var
+pp MiClase.new.mensaje_1
+
+pp MiClase2.new.mensaje_1
+
 
 #MiClase.new.mensaje_2
 # Retorna 3 e imprime:
