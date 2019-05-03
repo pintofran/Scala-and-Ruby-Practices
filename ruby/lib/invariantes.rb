0c3a7b75a6f43c828e6b157013ed2d4de0ab01f4
pintofran <francopinto7@gmail.com>
2019-05-03 19:39:07 -0300

    Falta invariantes en new

--- ruby/lib/invariantes.rb
+++ ruby/lib/invariantes.rb
@@ -1,36 +1,49 @@
 load 'prueba.rb'
 
-class Guerrero < Contrato
+class Guerrero
 
   before_and_after_each_call(
       # Bloque Before. Se ejecuta antes de cada mensaje
-      proc{ puts 'Primero' },
+      proc{ puts vida },
       # Bloque After. Se ejecuta después de cada mensaje
       proc{ puts 'Dps' }
   )
 
   attr_accessor :vida, :fuerza
 
-  invariant { puts vida }
-
+  invariant { vida > 20 }
+  invariant { fuerza > 20 }
 
   def initialize(vida)
     @vida = vida
+    @fuerza = 21
   end
 
   def atacar(otro)
     otro.vida -= fuerza
   end
 
-  def loco
-    puts 'loco'
-  end
-
+  pre { vida > 50 }
+  post { |result| result == 5 }
   def mensaje
      puts 'Mensaje'
+     return 5
+  end
+
+  def mensaje2
+    puts 'Mensaje'
+    return 6
+  end
+
+  post { |result| result == 5 }
+  def mensaje3
+    puts 'Mensaje'
+    return 5
   end
 
 end
 
-guer = Guerrero.new(50)
+guer = Guerrero.new(51)
+guer.mensaje2
 guer.mensaje
+guer.mensaje3
