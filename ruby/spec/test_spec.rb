6cb6ddd32ab9392c32fb041b1d64caf0b3c87bf5
Santiago Balay <santiagobalay@Santiagos-MacBook-Pro.local>
2019-05-08 13:51:12 -0300

    Agregados Tests before and after, faltaria agregar mas en este y hacer los de Invariants y Pre/Posta

--- ruby/spec/test_spec.rb
+++ ruby/spec/test_spec.rb
@@ -1,9 +1,44 @@
-describe Prueba do
-  let(:prueba) { Prueba.new }
+require_relative "../lib/prueba"
 
-  describe '#materia' do
-    it 'debería pasar este test' do
-      expect(prueba.materia).to be :tadp
+describe "before_and_after_each_call" do
+  context "Ya en Module" do
+
+    class Testing
+
+      attr_accessor :contador, :se_ejecuto_after, :se_ejecuto_before
+
+      before_and_after_each_call(
+          Proc.new{self.se_ejecuto_after = true},
+          Proc.new{self.se_ejecuto_before = true})
+
+      def initialize
+        self.contador = 0
+        self.se_ejecuto_after = false
+        self.se_ejecuto_before = false
+      end
+
+      def mensaje1
+        @contador = @contador + 1
+      end
     end
+
+    it "Debería ejecutar el proc 'before' cuando se llama a un metodo" do
+      test = Testing.new
+      test.mensaje1
+      expect(test.se_ejecuto_after).to be true
+      expect(test.contador).to be 1
+    end
+
+    it "Debería ejecutar el proc 'after' cuando se llama a un metodo" do
+      test = Testing.new
+      test.mensaje1
+      expect(test.se_ejecuto_before).to be true
+      expect(test.contador).to be 1
+    end
+
+
+
   end
+
 end
+
