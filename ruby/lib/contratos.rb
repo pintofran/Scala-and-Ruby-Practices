fc3e01a98fdc434f204a0c26158da9d199752097
pintofran <francopinto7@gmail.com>
2019-05-17 21:14:24 -0300

    Arregla repeticion de logica

--- ruby/lib/contratos.rb
+++ ruby/lib/contratos.rb
@@ -24,27 +24,27 @@ class Module
 
   def invariant(&bloque)
     init
-    @invariants.agregar_bloque_condicion(&bloque)
+    proc = condicional_proc("Error de consistencia de la clase",ErrorConsistencia,&bloque)
+    @invariants.agregar_bloque(proc)
   end
 
   def pre(&bloque)
     init
-    proc_nuevo = proc { |*var,this|
-      unless this.instance_exec(*var, &bloque)
-        raise ErrorPrePost, "Error de pre condicion"
-      end
-    }
-    @pre_condicion = proc_nuevo
+    @pre_condicion = condicional_proc('Error de pre condicion',ErrorPrePost,&bloque)
   end
 
   def post(&bloque)
     init
-    proc_nuevo = proc { |*var,this|
+    @post_condicion = condicional_proc('Error de post condicion',ErrorPrePost,&bloque)
+  end
+
+
+  def condicional_proc(mensaje_error,tipo_error,&bloque)
+    return proc { |*var,this|
       unless this.instance_exec(*var, &bloque)
-        raise ErrorPrePost, "Error de post condicion"
+        raise tipo_error, mensaje_error
       end
     }
-    @post_condicion = proc_nuevo
   end
 
   def ejecutarSinLoop(this,var,&proc)
@@ -72,12 +72,10 @@ class Module
       post_proc = proc { |*args| true}
     end
 
-    if @new_method && name != 'initialize'.to_sym
+    if @new_method
       @new_method = false
 
       old_method = instance_method(name)
-
-      parameter_names = old_method.parameters.map {|lista| lista.drop(1).pop}
 
       #Sobreescribimos el metodo
       define_method(name) do |*arg|
@@ -87,35 +85,32 @@ class Module
         dictionary = Hash[parameter_names.zip parameter_values]
         dictionary = dictionary.filter {|key,value| key.to_s != "element"}
 
-        self.singleton_class.class_eval { attr_accessor *(dictionary.keys.map {|key| key.to_s }) }
-
+        TempVars.crear_singleton_accesors_temporales(self,*dictionary.keys.map {|key| key.to_s })
         dictionary.each {|key, value|
           self.send(key.to_s++'=', value)
         }
 
         self.class.ejecutarSinLoop(self,nil,&pre_proc)
 
-        dictionary.each {|key, value|
-         self.singleton_class.remove_method((key.to_s++'=').to_sym)
-         self.singleton_class.remove_method(key)
-        }
+        TempVars.borrar_ultimos_accesors(self)
 
+        unless name == 'initialize'.to_sym
         self.class.bloques_before.ejecutar(self)
+        end
 
         var = old_method.bind(self).call(*arg)
 
         self.class.invariants.ejecutar(self)
 
+        unless name == 'initialize'.to_sym
         self.class.bloques_after.ejecutar(self)
+        end
 
-        self.singleton_class.class_eval { attr_accessor *(dictionary.keys.map {|key| key.to_s }) }
+        TempVars.crear_singleton_accesors_temporales(self,*dictionary.keys.map {|key| key.to_s })
 
         self.class.ejecutarSinLoop(self,var,&post_proc)
 
-        dictionary.each {|key, value|
-          self.singleton_class.remove_method((key.to_s++'=').to_sym)
-          self.singleton_class.remove_method(key)
-        }
+        TempVars.borrar_ultimos_accesors(self)
 
         return var
 
@@ -124,30 +119,23 @@ class Module
       @pre_condicion = nil
       @post_condicion = nil
       @new_method = true
-
-    else
-      if @new_method && name == 'initialize'.to_sym
-        @new_method = false
-
-        old_method = instance_method(name)
-
-        define_method(name) do |*arg|
-
-          self.class.ejecutarSinLoop(self,nil,&pre_proc)
-
-          var = old_method.bind(self).call(*arg)
-
-          self.class.invariants.ejecutar(self)
-          self.class.ejecutarSinLoop(self,var,&post_proc)
-
-          return var
-
-        end
-        @pre_condicion = nil
-        @post_condicion = nil
-        @new_method = true
-      end
     end
+  end
+end
+
+class TempVars
+
+  def self.crear_singleton_accesors_temporales(this,*keys)
+    @@keys=keys
+    this.singleton_class.class_eval { attr_accessor *keys }
+  end
+
+  def self.borrar_ultimos_accesors(this)
+      @@keys.each { |key|
+        this.singleton_class.remove_method((key.to_s++'=').to_sym)
+        this.singleton_class.remove_method(key)
+      }
+    @@keys = []
   end
 end
 
@@ -161,17 +149,6 @@ class BloquesEjecutables
   def agregar_bloque(nuevo_bloque)
 
     @bloques.push(nuevo_bloque)
-
-  end
-
-  def agregar_bloque_condicion(&bloque)
-
-    proc_modificado = proc {
-      unless self.instance_eval(&bloque)
-        raise ErrorConsistencia, "Error de consistencia de la clase"
-      end
-    }
-    @bloques.push(proc_modificado)
 
   end
 
