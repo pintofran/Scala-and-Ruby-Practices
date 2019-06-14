d14a3fe21ac72483b756806ff46c1eee4f9d31e2
-NicolasStupfler <NicolasStupfler@gmail.com>
2019-06-13 22:44:35 -0300

    aproach 2

--- scala/src/main/scala/MusicParser.scala
+++ scala/src/main/scala/MusicParser.scala
@@ -5,7 +5,7 @@ import scala.collection.mutable.ListBuffer
 case class Note(name: String)
 
 class MusicParser(input: String) {
-  protected val inputStream = new PushbackReader(new StringReader(parsear(input)))
+  protected val inputStream = new PushbackReader(new StringReader(input))
 
   protected def parseChar(): Char = {
     val parsed = inputStream.read()
@@ -13,13 +13,79 @@ class MusicParser(input: String) {
     return parsed.toChar
   }
 
-  protected def parseNote(): Nota = {
-    var next: Char = ' '
-    do next = parseChar() while (next == ' ')
+  protected def parseNote(next: Char): Nota = {
     Nota.notas.find(_.toString == next.toString()).getOrElse(throw new NotANoteException(next))
   }
 
-  def parse(): List[Nota] = {
+  protected def isNote(char: Char): Boolean = {
+    Nota.notas.toString.contains(char)
+  }
+
+  protected def parseMelody(): List[Nota] = {
+    var next: Char = ' '
+    do next = parseChar() while (next == ' ')
+    if (next.isDigit) {
+      return parsePattern(next)
+    }
+    if (isNote(next)) {
+      val note = parseNote(next)
+      return List[Nota](note)
+    }
+    throw new NotANoteOrDigitException(next)
+  }
+
+  protected def parsePattern(firstDigit: Char): List[Nota] = {
+    try {
+      var next: Char = ' '
+      val repetitionCharList: ListBuffer[Char] = ListBuffer()
+      repetitionCharList += firstDigit
+      do { 
+        do next = parseChar() while (next == ' ')
+        repetitionCharList += next
+      }
+      while (!next.isDigit)
+      val repetitionString: String = repetitionCharList.mkString
+      if (next == 'x') {
+        val repetitionNumber: Int = repetitionString.toInt
+        if (repetitionNumber == 0) return []
+        return parsePatternWithRepetitionNumber(repetitionNumber)
+      }
+      throw new NotXException(next)
+    } catch {
+      case _: EOIParserException => 
+    }
+    throw new SintaxException()
+  }
+
+  protected def parsePatternWithRepetitionNumber(repetitionNumber: Int): List[Nota] = {
+    try {
+      var next: Char = ' '
+      do next = parseChar() while (next == ' ')
+      if (next.toString !== "(") {
+        throw new NotOpenBracketException(next)
+      }
+      var patternDeepLevel: Int = 1
+      val bracketPatternCharList: ListBuffer[Char] = ListBuffer()
+      do { 
+        do next = parseChar() while (next == ' ') 
+        if (next.toString == ")") patternDeepLevel --
+        if (next.toString == "(") patternDeepLevel ++
+        if (patternDeepLevel != 0) bracketPatternCharList += next
+      }
+      while (patternDeepLevel != 0) 
+      val bracketPattern: String = bracketPatternCharList.mkString
+      var completeBracketPattern: String = ""
+      for (i <- 1 to repetitionNumber){
+        completeBracketPattern = completeBracketPattern ++ bracketPattern
+      }
+      return new MusicParser(completeBracketPattern).parse()
+    } catch {
+      case _: EOIParserException => 
+    }
+    throw new NotCloseBracketException()
+  }
+
+def parse(): List[Nota] = {
     val result: ListBuffer[Nota] = ListBuffer()
     try while (true)
       result += parseNote()
@@ -29,46 +95,11 @@ class MusicParser(input: String) {
     return result.toList
   }
 
-  def parsearN(i: Int, string: String) : String = {
-    if(i == 1){
-      if(string.length <= 3)
-        return string
-      else if("ABCDEFG".contains(string.head))
-        return string.head.toString ++ parsearN(1,string.tail)
-      else if("(x)".contains(string.head) || string.head.isDigit){
-        var temp = string.drop(3)
-        return parsearN(string.head.asDigit,obtenerPrincipio(temp)) ++ parsearN(1,temp.drop(obtenerPrincipio(temp).length + 1))
-      }else
-        throw new NotANoteException(string.head)
-    }else{
-      return parsearN(1,string) ++ parsearN(i-1,string)
-    }
-    return string
-  }
-
-  def parsear(string : String) = parsearN(1,string.filter(_ != ' '))
-
-  def obtenerPrincipio(input : String): String = {
-    var resultado: String = ""
-    var parentesisA: Int = 1
-
-    var i = 0
-    while(parentesisA != 0){
-      if(input.charAt(i) == '(')
-        parentesisA += 1
-      else if(input.charAt(i) == ')')
-        parentesisA -= 1
-
-      resultado ++= input.charAt(i).toString
-      i+=1
-    }
-    return resultado.init
-  }
-
-}
-
-
-
 class ParserException(reason: String) extends Exception(reason)
 class EOIParserException extends ParserException("reached end of input")
 class NotANoteException(val read: Char) extends ParserException(s"Expected [A|B|C|D|E|F|G] but got $read")
+class NotANoteOrDigitException(val read: Char) extends ParserException(s"Expected [A|B|C|D|E|F|G] or DIGIT but got $read")
+class NotXException(val read: Char) extends ParserException(s"Expected x but got $read")
+class NotOpenBracketException(val read: Char) extends ParserException(s"Expected open bracket but got $read")
+class NotCloseBracketException() extends ParserException(s"Unexpected end of bracket, missing closing bracket")
+class SintaxException() extends ParserException(s"Sintax Error")
