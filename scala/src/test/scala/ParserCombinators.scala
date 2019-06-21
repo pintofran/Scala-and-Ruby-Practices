843f0125dc27f5c0dfc0d0688569d8dec84894c5
pintofran <francopinto7@gmail.com>
2019-06-21 20:08:55 -0300

    Arregla tests

--- scala/src/test/scala/ParserCombinators.scala
+++ scala/src/test/scala/ParserCombinators.scala
@@ -1,4 +1,4 @@
-import ParserCombinator.{CharResult, ParseError, Parser}
+import ParserCombinator._
 import org.scalatest.{FreeSpec, Matchers}
 
 import scala.util.{Success, Try}
@@ -17,11 +17,11 @@ class ParserCombinatorTest extends FreeSpec with Matchers {
       }
 
       "Successful when parses the first char when fed with text" in {
-        assert(ParserCombinator.anyChar.parser("Some Test Text") == Try(CharResult('S', "ome Test Text")))
+        assert(ParserCombinator.anyChar.parse("Some Test Text") == Try(CharResult('S', "ome Test Text")))
       }
 
       "Successful when parses the first char when fed with only one char text" in {
-        assert(ParserCombinator.anyChar.parser("A") == Try(CharResult('A',"")))
+        assert(ParserCombinator.anyChar.parse("A") == Try(CharResult('A',"")))
       }
 
     }
@@ -29,16 +29,16 @@ class ParserCombinatorTest extends FreeSpec with Matchers {
     "Char Parser" - {
 
       "Fails when fed empty text" in {
-        val parseador :Parser = ParserCombinator.char
+        val parseador :Parser = ParserCombinator.anyChar
         assertThrows[ParseError] (parseador.parse("").get)
       }
 
       "Fails when parses another character than expected" in {
-        assert(ParserCombinator.char.parser('A', "Some Test Text") == Try(CharResult('S', "ome Test Text")))
+        assert(ParserCombinator.char('A').parse("Some Test Text") == Try(CharResult('S', "ome Test Text")))
       }
 
       "Successful when parses the expected character of the given text" in {
-        assert(ParserCombinator.char.parser('S', "Some Test Text") == Try(CharResult('S', "ome Test Text")))
+        assert(ParserCombinator.char('A').parse("Some Test Text") == Try(CharResult('S', "ome Test Text")))
       }
 
     }
@@ -51,11 +51,11 @@ class ParserCombinatorTest extends FreeSpec with Matchers {
       }
 
       "Successful when parses as Unit the first char of the given text" in {
-        assert(ParserCombinator.void.parser("Some Test Text") == Try(UnitResult("ome Test Text")))
+        assert(ParserCombinator.void.parse("Some Test Text") == Try(UnitResult("ome Test Text")))
       }
 
       "Successful when parses as Unit the first char when fed with only one char text" in {
-        assert(ParserCombinator.void.parser("A") == Try(UnitResult("")))
+        assert(ParserCombinator.void.parse("A") == Try(UnitResult("")))
       }
 
     }
@@ -78,15 +78,15 @@ class ParserCombinatorTest extends FreeSpec with Matchers {
       }
 
       "Successful when parses the first char as letter of the given text" in {
-        assert(ParserCombinator.letter.parser("Some Test Text") == Try(CharResult('S', "ome Test Text")))
+        assert(ParserCombinator.letter.parse("Some Test Text") == Try(CharResult('S', "ome Test Text")))
       }
 
       "Successful when parses the first char when fed with only one letter char text" in {
-        assert(ParserCombinator.letter.parser("A") == Try(CharResult('A', "")))
+        assert(ParserCombinator.letter.parse("A") == Try(CharResult('A', "")))
       }
 
       "Successful when parses the first char as letter with mixed fed text" in {
-        assert(ParserCombinator.letter.parser("S0M3 73$7 73X7") == Try(CharResult('S', "0M3 73$7 73X7")))
+        assert(ParserCombinator.letter.parse("S0M3 73$7 73X7") == Try(CharResult('S', "0M3 73$7 73X7")))
       }
 
     }
@@ -109,15 +109,15 @@ class ParserCombinatorTest extends FreeSpec with Matchers {
       }
 
       "Successful when parses the first char as digit of the given text" in {
-        assert(ParserCombinator.digit.parser("1234") == Try(CharResult('1', "234")))
+        assert(ParserCombinator.digit.parse("1234") == Try(CharResult('1', "234")))
       }
 
       "Successful when parses the first char when fed with only one digit char text" in {
-        assert(ParserCombinator.digit.parser("1") == Try(CharResult('1', "")))
+        assert(ParserCombinator.digit.parse("1") == Try(CharResult('1', "")))
       }
 
       "Successful when parses the first char as digit with mixed fed text" in {
-        assert(ParserCombinator.digit.parser("5ome Test Text") == Try(CharResult('5', "ome Test Text")))
+        assert(ParserCombinator.digit.parse("5ome Test Text") == Try(CharResult('5', "ome Test Text")))
       }
 
     }
@@ -135,27 +135,27 @@ class ParserCombinatorTest extends FreeSpec with Matchers {
       }
 
       "Successful when parses the first char as digit of the given text" in {
-        assert(ParserCombinator.alphaNum.parser("1234") == Try(CharResult('1', "234")))
+        assert(ParserCombinator.alphaNum.parse("1234") == Try(CharResult('1', "234")))
       }
 
       "Successful when parses the first char when fed with only one digit char text" in {
-        assert(ParserCombinator.alphaNum.parser("1") == Try(CharResult('1', "")))
+        assert(ParserCombinator.alphaNum.parse("1") == Try(CharResult('1', "")))
       }
 
       "Successful when parses the first char as digit with mixed fed text" in {
-        assert(ParserCombinator.alphaNum.parser("5ome Test Text") == Try(CharResult('5', "ome Test Text")))
+        assert(ParserCombinator.alphaNum.parse("5ome Test Text") == Try(CharResult('5', "ome Test Text")))
       }
 
       "Successful when parses the first char as letter of the given text" in {
-        assert(ParserCombinator.alphaNum.parser("Some Test Text") == Try(CharResult('S', "ome Test Text")))
+        assert(ParserCombinator.alphaNum.parse("Some Test Text") == Try(CharResult('S', "ome Test Text")))
       }
 
       "Successful when parses the first char when fed with only one letter char text" in {
-        assert(ParserCombinator.alphaNum.parser("A") == Try(CharResult('A', "")))
+        assert(ParserCombinator.alphaNum.parse("A") == Try(CharResult('A', "")))
       }
 
       "Successful when parses the first char as letter with mixed fed text" in {
-        assert(ParserCombinator.alphaNum.parser("S0M3 73$7 73X7") == Try(CharResult('S', "0M3 73$7 73X7")))
+        assert(ParserCombinator.alphaNum.parse("S0M3 73$7 73X7") == Try(CharResult('S', "0M3 73$7 73X7")))
       }
 
     }
@@ -163,21 +163,21 @@ class ParserCombinatorTest extends FreeSpec with Matchers {
     "String Parser" - {
 
       "Fails when fed empty text" in {
-        val parseador :Parser = ParserCombinator.string
+        val parseador :Parser = ParserCombinator.string("test")
         assertThrows[ParseError] (parseador.parse("").get)
       }
 
       "Fails when parses another string than expected" in {
-        val parseador :Parser = ParserCombinator.string
-        assertThrows[ParseError] (parseador.parse("Some Failure Text", "Some Test Text").get)
+        val parseador :Parser = ParserCombinator.string("Some Failure Text")
+        assertThrows[ParseError] (parseador.parse("Some Test Text").get)
       }
 
       "Successful when parses the expected string of a exact given text" in {
-        assert(ParserCombinator.string.parser("Some", "Some Test Text") == Try(StringResult("Some", " Test Text")))
+        assert(ParserCombinator.string("Some").parse("Some Test Text") == Try(StringResult("Some", " Test Text")))
       }
 
       "Successful when parses the expected string of the given text" in {
-        assert(ParserCombinator.string.parser("Some Test Text", "Some Test Text") == Try(StringResult("Some Test Text", "")))
+        assert(ParserCombinator.string("Some Test Text").parse("Some Test Text") == Try(StringResult("Some Test Text", "")))
       }
 
     }
@@ -191,16 +191,16 @@ class ParserCombinatorTest extends FreeSpec with Matchers {
 
       "Fails when both parser fails" in {
         assertThrows[ParseError] {
-          ParserCombinator.aob.parser("que tal").get
+          ParserCombinator.aob.parse("que tal").get
         }
       }
 
       "Successful when first parser is successful" in {
-        assert(ParserCombinator.aob.parser("arbol") == Try(CharResult('a',"rbol")))
+        assert(ParserCombinator.aob.parse("arbol") == Try(CharResult('a',"rbol")))
       }
 
       "Successful when first parser fails but second parser is successful" in {
-        assert(ParserCombinator.aob.parser("bort") == Try(CharResult('b',"ort")))
+        assert(ParserCombinator.aob.parse("bort") == Try(CharResult('b',"ort")))
       }
     }
 
@@ -209,22 +209,22 @@ class ParserCombinatorTest extends FreeSpec with Matchers {
 
       "Fails when first parser fails" in {
         assertThrows[ParseError] {
-          ParserCombinator.holaMundo.parser("holimundo").get
+          ParserCombinator.holaMundo.parse("holimundo").get
         }
       }
 
       "Fails when second parser fails" in {
         assertThrows[ParseError] {
-          ParserCombinator.holaMundo.parser("holamundu").get
+          ParserCombinator.holaMundo.parse("holamundu").get
         }
       }
 
       "Successful when both parsers success" in {
-        assert(ParserCombinator.holaMundo.parser("holamundo") == Try(StringResult("holamundo","")))
+        assert(ParserCombinator.holaMundo.parse("holamundo") == Try(StringResult("holamundo","")))
       }
 
       "Successful when both parsers success with remaining text" in {
-        assert(ParserCombinator.holaMundo.parser("holamundo!") == Try(StringResult("holamundo","!")))
+        assert(ParserCombinator.holaMundo.parse("holamundo!") == Try(StringResult("holamundo","!")))
       }
 
     }
@@ -234,22 +234,22 @@ class ParserCombinatorTest extends FreeSpec with Matchers {
 
       "Fails when first parser fails" in {
         assertThrows[ParseError] {
-          ParserCombinator.holaMundo.parser("holimundo").get
+          ParserCombinator.holaMundo.parse("holimundo").get
         }
       }
 
       "Fails when second parser fails" in {
         assertThrows[ParseError] {
-          ParserCombinator.holaMundo.parser("holamundu").get
+          ParserCombinator.holaMundo.parse("holamundu").get
         }
       }
 
       "Successful when both parsers success" in {
-        assert(ParserCombinator.holaMundo.parser("holamundo") == Try(StringResult("mundo","")))
+        assert(ParserCombinator.holaMundo.parse("holamundo") == Try(StringResult("mundo","")))
       }
 
       "Successful when both parsers success with remaining text" in {
-        assert(ParserCombinator.holaMundo.parser("holamundo!") == Try(StringResult("mundo","!")))
+        assert(ParserCombinator.holaMundo.parse("holamundo!") == Try(StringResult("mundo","!")))
       }
 
     }
@@ -259,22 +259,22 @@ class ParserCombinatorTest extends FreeSpec with Matchers {
 
       "Fails when first parser fails" in {
         assertThrows[ParseError] {
-          ParserCombinator.holaMundo.parser("holimundo").get
+          ParserCombinator.holaMundo.parse("holimundo").get
         }
       }
 
       "Fails when second parser fails" in {
         assertThrows[ParseError] {
-          ParserCombinator.holaMundo.parser("holamundu").get
+          ParserCombinator.holaMundo.parse("holamundu").get
         }
       }
 
       "Successful when both parsers success" in {
-        assert(ParserCombinator.holaMundo.parser("holamundo") == Try(StringResult("hola","mundo")))
+        assert(ParserCombinator.holaMundo.parse("holamundo") == Try(StringResult("hola","mundo")))
       }
 
       "Successful when both parsers success with remaining text" in {
-        assert(ParserCombinator.holaMundo.parser("holamundo!") == Try(StringResult("hola","mundo!")))
+        assert(ParserCombinator.holaMundo.parse("holamundo!") == Try(StringResult("hola","mundo!")))
       }
 
     }
@@ -284,26 +284,26 @@ class ParserCombinatorTest extends FreeSpec with Matchers {
 
       "Fails when separator parser fails" in {
         assertThrows[ParseError] {
-          ParserCombinator.inicialesNombresConBarras.parser("Juan-Perez").get
+          ParserCombinator.inicialesNombresConBarras.parse("Juan-Perez").get
         }
       }
 
       "Fails when container parser fails" in {
         assertThrows[ParseError] {
-          ParserCombinator.inicialesNombresConBarras.parser("Juan/1erez").get
+          ParserCombinator.inicialesNombresConBarras.parse("Juan/1erez").get
         }
       }
 
       "Successful when both parsers success" in {
-        assert(ParserCombinator.inicialesNombresConBarras.parser("Juan/Perez") == [Try(CharResult('J',"uan")), Try(CharResult('P',"erez")) ])
+        assert(ParserCombinator.inicialesNombresConBarras.parse("Juan/Perez") == [Try(CharResult('J',"uan")), Try(CharResult('P',"erez")) ])
       }
 
       "Successful with one iterations" in {
-        assert(ParserCombinator.inicialesNombresConBarras.parser("Juan") == [Try(CharResult('J',"uan"))])
+        assert(ParserCombinator.inicialesNombresConBarras.parse("Juan") == [Try(CharResult('J',"uan"))])
       }
 
       "Successful with multiple iterations" in {
-        assert(ParserCombinator.inicialesNombresConBarras.parser("Juan/Martin/Perez/Dias") == [Try(CharResult('J',"uan")), Try(CharResult('M',"artin")), Try(CharResult('P',"erez")), Try(CharResult('D',"ias")) ])
+        assert(ParserCombinator.inicialesNombresConBarras.parse("Juan/Martin/Perez/Dias") == [Try(CharResult('J',"uan")), Try(CharResult('M',"artin")), Try(CharResult('P',"erez")), Try(CharResult('D',"ias")) ])
       }
     }
 
